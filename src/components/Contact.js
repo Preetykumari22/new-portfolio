fce872d8d668d1da8d62e53d15414488289057fb
Omprakashkr1 <112559646+Omprakashkr1@users.noreply.github.com>
2024-11-07 12:05:33 +0530

    deploy

---
+++ src/components/Contact.js
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import { Container, Row, Col } from "react-bootstrap";
+
+import 'animate.css';
+
+export const Contact = () => {
+
+  return (
+    <section className="contact" id="connect">
+      <Container>
+
+      </Container>
+    </section>
+  )
+}
